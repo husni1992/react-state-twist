2a70eba780bc7c79bf2fcf5f2651d66ff5a69ada
H.A.Mohomed Gazzali <mgha123@gmail.com>
2024-05-06 12:44:07 +0200

    deployment config

---
+++ next.config.js
@@ -0,0 +1,11 @@
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+  output: "export",
+
+  // Optional: Change the output directory `out` -> `dist`
+  //   distDir: "dist",
+};
+
+module.exports = nextConfig;
